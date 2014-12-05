02ceb34a3b1cc760e02a44b3327d42a698bfd4c0
Vick Kelkar <vkelkar@pivotal.io>
2014-12-05 10:19:47 -0800

    Disable Ruby built-in documentation
    We should not need this during automated builds and it speeds up installation considerably
    
    Also disabled wget progress bar to clean up logs
    
    Signed-off-by: Adam Stegman <astegman@pivotal.io>

--- install_ruby2-1-2.sh
+++ install_ruby2-1-2.sh
@@ -5,10 +5,10 @@ rm -rf ruby-installer
 mkdir ruby-installer
 cd ruby-installer
 echo "fetch latest ruby from ftp.ruby-lang.org"
-wget http://ftp.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz
+wget -nv http://ftp.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz
 tar -xzvf ruby-2.1.2.tar.gz
 echo "installing ruby 2.1.2"
-cd ruby-2.1.2/ && ./configure && make && make install
+cd ruby-2.1.2/ && ./configure --disable-install-doc && make && make install
 echo "installed ruby interpreter, version:"
 echo `ruby -v`
 gem install bundler --no-ri --no-rdoc
