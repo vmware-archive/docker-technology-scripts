880c5b408a3c3d4caa70add11a4a16023bbd6d84
Greg Ziebold <gziebold@pivotal.io>
2014-12-17 11:57:25 -0700

    Add all dependencies for the bosh gems
    Signed-off-by: Mike Gehard <mgehard@pivotal.io>

--- install_bosh_cli.sh
+++ install_bosh_cli.sh
@@ -2,12 +2,15 @@
 
 apt-get update
 
-apt-get install -y libxslt-dev libxml2-dev
+# The DB clients are required for the aws plugin
+apt-get install -y libxslt-dev libxml2-dev libmysqlclient-dev libpq-dev libsqlite3-dev
 
 gem install bosh_cli --no-ri --no-rdoc
 
 # Plugin required for 'bosh aws create' and bootstrap commands
+# Still unclear if we need this
 gem install bosh_cli_plugin_aws --no-ri --no-rdoc
 
 # Plugin required for deploying MicroBOSH
+# Still unclear if we need this
 gem install bosh_cli_plugin_micro --no-ri --no-rdoc
