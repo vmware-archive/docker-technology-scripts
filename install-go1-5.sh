789a6326c79574f0063c20f23a9c18bc164f08e2
Warren Fernandes <wfernandes@pivotal.io>
2016-01-20 13:56:08 -0700

    Upgrading install script to golang 1.5.3 from 1.5.1
    [#112074095]
    
    Signed-off-by: Rohit Kumar <rokumar@pivotal.io>

--- install-go1-5.sh
+++ install-go1-5.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz
-tar -xzf go1.5.1.linux-amd64.tar.gz -C /usr/local
+wget https://storage.googleapis.com/golang/go1.5.3.linux-amd64.tar.gz
+tar -xzf go1.5.3.linux-amd64.tar.gz -C /usr/local
 go get github.com/onsi/ginkgo/ginkgo golang.org/x/tools/cmd/vet golang.org/x/tools/cmd/cover
 
