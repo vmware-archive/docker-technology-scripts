985ab1d8a2c675768bc82d4d607f3bf4c1472717
Maya Rosecrance <mrosecrance@pivotal.io>
2016-04-08 11:29:36 -0600

    Remove go vet as its bundled in the binary
    Signed-off-by: Maya Rosecrance <mrosecrance@pivotal.io>

--- install-go1-6.sh
+++ install-go1-6.sh
@@ -2,5 +2,5 @@
 
 wget https://storage.googleapis.com/golang/go1.6.linux-amd64.tar.gz
 tar -xzf go1.6.linux-amd64.tar.gz -C /usr/local
-go get github.com/onsi/ginkgo/ginkgo golang.org/x/tools/cmd/vet golang.org/x/tools/cmd/cover
+go get github.com/onsi/ginkgo/ginkgo golang.org/x/tools/cmd/cover
 
