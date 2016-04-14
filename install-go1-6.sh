ddf09f3317e1de9ed72834b037418cb884bd501a
Kira Combs <kcombs@pivotal.io>
2016-04-14 13:48:50 -0600

    Update 1.6 install to 1.6.1 [#117636797]
    Signed-off-by: Tom Chen <tochen@pivotal.io>

--- install-go1-6.sh
+++ install-go1-6.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget https://storage.googleapis.com/golang/go1.6.linux-amd64.tar.gz
-tar -xzf go1.6.linux-amd64.tar.gz -C /usr/local
+wget https://storage.googleapis.com/golang/go1.6.1.linux-amd64.tar.gz
+tar -xzf go1.6.1.linux-amd64.tar.gz -C /usr/local
 go get github.com/onsi/ginkgo/ginkgo golang.org/x/tools/cmd/cover
 
