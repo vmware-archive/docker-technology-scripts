74e4cd98bc3a68797eba8364a64c26157014e6f0
Jason Keene <jkeene@pivotal.io>
2016-08-24 13:24:26 -0600

    Add go1.7 installation script
    [#128422331]
    
    Signed-off-by: nino khodabandeh <ninok@hpe.com>

---
+++ install-go1-7.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz
+tar -xzf go1.7.linux-amd64.tar.gz -C /usr/local
+go get github.com/onsi/ginkgo/ginkgo golang.org/x/tools/cmd/cover
